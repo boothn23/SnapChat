9a498369425ab9b879c279a3ef5fa6bc4cb92903
njbooth04 <njbooth04@icloud.com>
2023-02-28 20:07:18 -0500

    It works

--- app/src/main/java/com/example/snapchat/MainFragment.kt
+++ app/src/main/java/com/example/snapchat/MainFragment.kt
@@ -11,16 +11,15 @@ class MainFragment : Fragment() {
     private var _binding: FragmentMainBinding? = null
     private val binding get() = _binding!!
 
+    val snaps = listOf(
+        com.example.snapchat.Snap("Al Gore", "3m", R.drawable.joe, false),
+        Snap("Gary Johnson", "8h", R.drawable.joe, true),
+        Snap("Ross Perot", "3d", R.drawable.joe, false),
+    )
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         _binding = FragmentMainBinding.inflate(inflater, container, false)
         val rootView = binding.root
-
-        val snaps = listOf(
-            Snap("Al Gore", "3m", R.drawable.joe, false),
-            Snap("Gary Johnson", "8h", R.drawable.joe, true),
-            Snap("Ross Perot", "3d", R.drawable.joe, false),
-        )
-
 
         val myAdapter = SnapAdapter(snaps)
         binding.recyclerView.adapter = myAdapter
